7e98b77482b437b6b67cf901ac23811dd6d63141
Apollinaria Chernikova <74719407+ApollyCh@users.noreply.github.com>
2023-06-22 12:52:54 +0300

    updated

---
+++ index.js
@@ -0,0 +1,27 @@
+const insta = document.getElementById("inst")
+insta.addEventListener('click', () => {
+    location.href = "https://instagram.com/apollinaria_chernikova?igshid=MjEwN2IyYWYwYw==";
+})
+
+const pint = document.getElementById("pint")
+pin.addEventListener('click', () => {
+    location.href = "https://www.pinterest.com/apollinariachernikova/_created/";
+})
+
+const git = document.getElementById("git")
+git.addEventListener('click', () => {
+    location.href = "https://github.com/ApollyCh";
+})
+
+const tel = document.getElementById("tel")
+tel.addEventListener('click', () => {
+    location.href = "https://t.me/Apollinaria2004";
+})
+
+const work = document.getElementById("b2")
+work.addEventListener('click', () => {
+    window.scrollTo({
+        top: 1000,
+        behavior: "smooth"
+    });
+})
